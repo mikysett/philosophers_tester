1c3212cd7fde0b52862e77ab2c27aa6159d97203
Michele Sessa <msessa@c2r3s3.42lisboa.com>
2021-08-04 18:56:35 +0100

    add some tests for bonus, the tester is still not checked for bonuses since I will need my philo_bonus to test it

--- init_tests.sh
+++ init_tests.sh
@@ -24,7 +24,7 @@ cmd+=("$tester_pathname NO_BONUS 2 400 100 400")
 
 cmd+=("$tester_pathname NO_BONUS 2 400 300 200")
 
-# EXTRA TESTS
+# # EXTRA TESTS
 
 cmd+=("$tester_pathname NO_BONUS 4 300 100 100")
 
@@ -46,28 +46,20 @@ cmd+=("$tester_pathname NO_BONUS 20 400 100 100")
 
 # TESTS FOR BONUS
 
-# cmd+=("$tester_pathname BONUS 1 200 100 100")
+cmd+=("$tester_pathname BONUS 1 800 200 200")
 
-# cmd+=("$tester_pathname BONUS 2 200 100 100")
+cmd+=("$tester_pathname BONUS 5 800 200 200")
 
-# cmd+=("$tester_pathname BONUS 2 400 100 100")
+cmd+=("$tester_pathname BONUS 5 800 200 200 7")
 
-# cmd+=("$tester_pathname BONUS 4 300 100 100")
+cmd+=("$tester_pathname BONUS 4 410 200 200")
 
-# cmd+=("$tester_pathname BONUS 4 300 20 100")
+cmd+=("$tester_pathname BONUS 4 310 200 100")
 
-# cmd+=("$tester_pathname BONUS 4 300 200 10")
+cmd+=("$tester_pathname BONUS 2 200 100 100")
 
-# cmd+=("$tester_pathname BONUS 4 500 200 10")
+cmd+=("$tester_pathname BONUS 2 400 100 100")
 
-# cmd+=("$tester_pathname BONUS 5 300 20 100")
+cmd+=("$tester_pathname BONUS 2 400 100 400")
 
-# cmd+=("$tester_pathname BONUS 5 300 200 10")
-
-# cmd+=("$tester_pathname BONUS 5 500 200 10")
-
-# cmd+=("$tester_pathname BONUS 10 400 100 100")
-
-# cmd+=("$tester_pathname BONUS 20 400 100 100")
-
-# cmd+=("$tester_pathname BONUS 20 400 1 100")
+cmd+=("$tester_pathname BONUS 2 400 300 200")
